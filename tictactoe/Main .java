cc41f44ffeab2abb66e68b34b2e75a0427dc0861
NaveenMandadi444 <naveenmandadi444@gmail.com>
2024-12-18 19:18:44 +0530

    Update Main .java

--- tictactoe/Main .java
+++ tictactoe/Main .java
@@ -1,8 +1,21 @@
-package com.tictactoe;
+import java.util.*;
 
- class Main {
-    public static void main(String[] args) {
-        TicTacToeGame game = new TicTacToeGame();
-        game.play();
-    }
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+     System.out.println("enter playing Mode (Normal-1|advanced-2)");
+
+     int level=sc.nextInt();
+     PlayMode pm;
+     switch(level){
+      case 1:pm=new NormalMode();
+            pm.play();
+            break;
+      case 2: pm=new AdvancedMode();
+            pm.play();  
+            break;
+      default:System.out.println("Plz Enter valid Levels Only (1|2)");
+      
+     }
+  }
 }
